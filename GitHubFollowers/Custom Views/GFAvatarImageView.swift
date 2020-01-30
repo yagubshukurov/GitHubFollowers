be63490791d4b521ef8869c35fa427622ae86e96
Yagub Shukurov <shukurov23@outlook.com>
2020-01-30 23:59:57 +0100

    DiffableDataSource

---
+++ GitHubFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,21 @@
+//
+//  GFAvatarImageView.swift
+//  GitHubFollowers
+//
+//  Created by Yagub Shukurov on 1/30/20.
+//  Copyright © 2020 Yagub Shukurov. All rights reserved.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
